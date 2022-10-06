0fb2e3c21b6bbc326965468818f1bfc8803a9e38
Israel <Israel@Israel-PC>
2022-10-05 21:21:02 -0400

    clase creada en rama de daniel

---
+++ src/equipos/futbol2/RamadePruebaDaniel.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package equipos.futbol2;
+
+/**
+ *
+ * @author Israel
+ */
+public class RamadePruebaDaniel {
+    /*
+    comentarios de la clase rama daniel
+    */
+}
