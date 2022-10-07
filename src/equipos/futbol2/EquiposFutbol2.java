1e2b6527db53bd29c5b8d407f0cde8b66c0bfae2
Israel <Israel@Israel-PC>
2022-10-06 21:33:46 -0400

    version1 proyecto

--- src/equipos/futbol2/EquiposFutbol2.java
+++ src/equipos/futbol2/EquiposFutbol2.java
@@ -4,6 +4,7 @@
  */
 package equipos.futbol2;
 
+import Vistas.vistaRegistroEquipos;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -20,51 +21,8 @@ public class EquiposFutbol2 {
      */
     public static void main(String[] args) {
         
-        
-        Connection con = null;
-        //String user = "root";
-        //String user = "jesux28";
-        //String password = "2812ale";
-        //String password = "";
-        //String url="jdbc:mysql://localhost:3306/liga";
-        //String url="jdbc:postgresql://192.168.100.11:5432/EQUIPO";
-        
-        try {
-            con = DriverManager.getConnection("jdbc:postgresql://192.168.100.11:5432/EQUIPO","jesux28","2812ale");
-            //con = DriverManager.getConnection(url,user,password);
-            
-            //2. CREAR OBJETO STATEMENT
-            Statement miStatement = (Statement) con.createStatement();
-            //3. EJECUTAR SQL// Nos devuelve el resultado de la Consulta
-            String consulta = "SELECT ID_EQUIPO,NOMBRE_EQUIPO FROM equipofutbol;";
-            ResultSet miResultSet = miStatement.executeQuery(consulta);
-            //4. RECORRER EL RESULT SET, el RS es una tabla en memoria Virtual
-
-            while(miResultSet.next()){ //Con el metodo next podemos avanzar registro
-                System.out.println(miResultSet.getInt("ID_EQUIPO")+miResultSet.getString("NOMBRE_EQUIPO"));
-            }
-            
-            
-            
-        } catch (SQLException ex) {
-            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        new vistaRegistroEquipos().setVisible(true);
     }
-        
-    /*
-    private  Connection miCon;
-    Imagen_Fondo ima = new Imagen_Fondo();
-    public CONEXIONBS() {
-        this.setContentPane(ima);
-        initComponents();
-        try{
-           //1. CREAR OBJETO CONNECTION 
-            miCon = DriverManager.getConnection("jdbc:postgresql://localhost:5432/REPV2","jesux28","2812ale");
-         }catch(Exception e){
-           System.out.println(e.toString());
-         }
-    }
-    */
    
     
 }
